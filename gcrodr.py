20d8c1c6105fed6bc2dc09d1c52a4d1c1b0d4e0b
Aaron Chu-Carroll <aaron.chucarroll@gmail.com>
2024-03-21 15:35:58 -0400

    init

---
+++ gcrodr.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# GCRO DR - GCRO with Defalted Restarting
+# X = GCRODR(A, B, M, K) attempts to solve the system of linear equations A*X = B for X
+# A must be an n x n coefficient matrix
+# B must be a column vector of length n
+# M is the maximum subspace dimension used by GCRODR
+# K is the number of approximate eigenvectors kept from one cycle to the next
+# OPTIONAL PARAMS:
+# x0 specifies an initial guess. The default is an all zero vector
+
+def gcrodr(A, b, m, k, x0):
+    x = np.zeros(len(x0))  #initialize solution vector
+    count = 1
+
+    r = b - A * x0
